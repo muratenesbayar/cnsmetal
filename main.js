2c824918b86818575b0d4e87a92630c3619defbc
Murat Enes Bayar <enes.murat.byr2@gmail.com>
2024-05-04 19:59:08 +0300

    new update

---
+++ main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      autoplay: {
+        delay: 1500,
+        disableOnInteraction: false
+      },
+      loop: true
+    });
